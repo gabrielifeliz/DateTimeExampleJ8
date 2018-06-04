ac6a4f8b1604f19018f1f5fa37f43ad9eb142aae
gabrielifeliz <gfeliz3@montgomerycollege.edu>
2018-06-04 13:51:38 -0400

    updates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ package com.company;
  
 Look at this example to see how easy working with dates can be:
 https://github.com/aoa4eva/DateTimeExampleJ8
- (Links to an external site.)
-Links to an external site.
  
 Now, modify that exercise to make it show the following:
 - The date entered in day, Month (3 letters) and year (4 digit) format
@@ -24,101 +22,49 @@ If the date the user enters  is NOT in dd/mm/yyyy format,
 
 
 import java.time.LocalDate;
-
 import java.time.LocalDateTime;
-
-import java.time.LocalTime;
-
-import java.time.Year;
-
 import java.time.format.DateTimeFormatter;
-
 import java.util.Scanner;
 
 
-
 public class Main {
-
-
 
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
 
-
-
         //Get the current time
-
-        LocalDateTime rightNow= LocalDateTime.now();
+        LocalDateTime rightNow = LocalDateTime.now();
 
         //Date from the user
-
-        LocalDate userDate=null;
-
-
+        LocalDate userDate = null;
 
         //Set up formatters so that you can use them later
-
+        DateTimeFormatter shortMonthFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        DateTimeFormatter shortMonthFormat2 = DateTimeFormatter.ofPattern("MMM dd yyyy");
         DateTimeFormatter dTF = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        DateTimeFormatter shortMonthFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
-
-        DateTimeFormatter longFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
-
-
-
         //Time formatter (time only)
-
         DateTimeFormatter hr24 = DateTimeFormatter.ofPattern("kk:m");
 
+        // Prompt the user for
+        System.out.print("Enter a date (dd/MM/yyyy): ");
+        String aDate = keyboard.nextLine();
+        userDate = LocalDate.parse(aDate, dTF);
 
+        while (userDate.compareTo(rightNow.toLocalDate()) > 0) {
+            System.out.println("Enter a date before today or today's date: ");
+            aDate = keyboard.nextLine();
+            userDate = LocalDate.parse(aDate, dTF);
+        }
 
-        //Output today's date in formats that have been preset
-
-        System.out.println("The current date is: "+rightNow.format(longFormat));
-
-        System.out.println("This is the current date and time unformatted: "+rightNow);
-
-
-
-        //Output the current time in formats that have been preset
-
-        System.out.println("This is the current system time: "+ LocalTime.now());
-
-        System.out.println("This is the current system time (24 h format): "+LocalTime.now().format(hr24));
-
-
-
-        //This is how you parse a string with a date time formatter
-
-        String aDate = "22/05/2010";
-
-        userDate = LocalDate.parse(aDate,dTF);
-
-        System.out.println(userDate.format(longFormat));
-
-
-
-
-
-
-
-        //Display the date entered
-
-        System.out.println(userDate.format(shortMonthFormat));
-
-
-
-        System.out.println("Now enter a year");
-
-        String thisYear = keyboard.nextLine();
-
-        Year newYear = Year.of(Integer.parseInt(thisYear));
-
-
-
-        System.out.println("This is the year entered:"+newYear);
+        System.out.println();
+        System.out.println("The date entered in day, Month (3 letters) " +
+                "and year (4 digit) format:" + "\n    " + userDate.format(shortMonthFormat));
+        System.out.println("The date entered in Month (3 letters), day  " +
+                "and year (4 digit) format:" + "\n    " + userDate.format(shortMonthFormat2));
+        System.out.println("The date entered in day, month " +
+                "(2 digits) and year format:" + "\n    " + userDate.format(dTF));
 
     }
-
 }
